209923ca3db5565dd3b94afb1c012d628d23431a
Chan-Mye <chanmye1984@gmail.com>
2018-01-29 15:01:19 +0800

    checking-n...

---
+++ webpack.config.prod.js
@@ -0,0 +1,33 @@
+import path from 'path';
+import webpack from 'webpack';
+
+export default {
+  debug: true,
+  devtool: 'source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  // devServer: {
+  //   contentBase: path.resolve(__dirname, 'src')
+  // },
+  plugins: [
+    // Eliminate duplicat packages when generating bundles
+    new webpack.optimize.DedupePlugin(),
+
+    // Minify js
+    new webpack.optimize.UglifyJsPlugin()
+  ],
+  module: {
+    'loaders': [
+      { test: /\.js$/, exclude: /node_modules/, loaders: ['babel'] },
+      { test: /\.css$/, loaders: ['style', 'css'] }
+    ]
+  }
+}
